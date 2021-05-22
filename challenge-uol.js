1f2bf9fd1ec28ec6c2040ed77c773338b9a4a421
Thiago Araujo <thiago.araujo72@etec.sp.gov.br>
2021-05-22 15:19:52 -0300

    alterando modo de conexão com o Mongo

--- challenge-uol.js
+++ challenge-uol.js
@@ -7,7 +7,7 @@ const logger = require('./src/lib/logger');
 (async function () {
     try {
         const server =  new Server();
-        const mongo = new MongoConnect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT || 27017}/${process.env.MONGO_SCHEMA}`);
+        const mongo = new MongoConnect(`${process.env.MONGO_HOST}/${process.env.MONGO_SCHEMA}`);
 
         await mongo.connect();
         server.listen();
